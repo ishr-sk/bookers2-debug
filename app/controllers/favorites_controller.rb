c2bc302585666c16432c53dac274815dfdcd7194
ishr-sk <kmrttnr1208@gmail.com>
2021-07-03 04:37:42 +0000

    [課題６-1完了]いいね機能非同期通信で実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
+    # 非同期通信化のため下記削除
     redirect_back(fallback_location: root_path)
   end
   
@@ -11,6 +12,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
+    # 非同期通信化のため下記削除
     redirect_back(fallback_location: root_path)
   end
   
